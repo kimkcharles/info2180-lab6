07e61906f25f4d286bb7d078da5325695f30e87d
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2018-11-13 03:34:18 +0000

    Execerise 4 Complete. Finished

--- apps.js
+++ apps.js
@@ -1,23 +1,31 @@
-window.onload = function() {
+window.onload = function(){
+   
     var search = document.getElementById("search_btn");
+    var getAllButton = document.querySelector('#all_btn');
     var httpRequest;
+    var xmlRequest;
     var response;
+    var result = document.querySelector('#results');
+    var lookup=document.getElementById("search_form").elements.namedItem("lookup_word");
     
     search.addEventListener("click", function(e) {
         e.preventDefault();
         
         httpRequest = new XMLHttpRequest();
-        var searchWord=document.getElementById("search_form").elements.namedItem("lookup_word").value;
-        var url = `request.php?q=${searchWord}`;
+        var searchWord=lookup.value.toLocaleLowerCase().trim();
+        var url = `request1.php?q=${searchWord}`;
         httpRequest.onreadystatechange = getDefn;
         httpRequest.open('GET', url);
         httpRequest.send();
     });
+    
     function getDefn(){
          if (httpRequest.readyState === XMLHttpRequest.DONE) {
              if (httpRequest.status === 200) {
+                 
+                
                 response = httpRequest.responseText;
-                var result = document.querySelector('#result');
+                
                 //var res=response.replace(/(<([^>]+)>)/ig,"");
                 //var res=striptags(response);
                 result.innerHTML=response;
@@ -27,13 +35,57 @@ window.onload = function() {
               }
          }
     }
-    function striptags(str){
-        str = str.replace("<p>","");
-        str = str.replace("</p>","\n");
-        str = str.replace("<h3>","");
-        str = str.replace("</h3>","\n");
-        return str;
+    
+    
+    // function striptags(str){
+    //     str = str.replace("<p>","");
+    //     str = str.replace("</p>","\n");
+    //     str = str.replace("<h3>","");
+    //     str = str.replace("</h3>","\n");
+    //     return str;
+    // }
+
+   
+   
+ 
+    getAllButton.addEventListener("click", function(element){
+        element.preventDefault();
+        var url = "request.php?q=&all=true";
+        xmlRequest = new XMLHttpRequest();
+        xmlRequest.onreadystatechange = displayAllDefinitions;
+        xmlRequest.open('GET',url,true);
+        xmlRequest.send();
+        
+    });
+    
+    function displayAllDefinitions(){
+        if(xmlRequest.readyState === XMLHttpRequest.DONE){
+            if(xmlRequest.status === 200){
+                $(result).empty();
+                let defns = xmlRequest.responseXML.getElementsByTagName('definition');
+                var list = document.createElement('ol');
+                for(let i=0;i<defns.length;i++){
+                    
+                    var def = document.createElement('LI');
+                    var h =  document.createElement('H3');
+                    var name = defns[i].getAttribute("name").toLocaleUpperCase();
+                    h.innerHTML = name;
+                    var p1 =  document.createElement('P');
+                    var t1 = defns[i].textContent;
+                    p1.innerHTML = t1;
+                    var p2 =  document.createElement('P');
+                    var t2 = `-${defns[i].getAttribute("author")}`;
+                    p2.innerHTML = t2;
+                    
+                    def.append(h);
+                    def.append(p1);
+                    def.append(p2);
+                    list.appendChild(def);
+                    
+                    result.appendChild(list);
+                };
+            }
+        }
     }
-}
-
-
+    
+};
--- request.php
+++ request.php
@@ -1,18 +1,31 @@
 <?php
 
-// accept a term (keyword)
-// respond with a value
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name = "definition" author = "Kim">
+    A statement of the exact meaning of a word, especially in a dictionary
+    </definition>
+    <definition name = "bar"  author = "Dan">
+    A place that sells alcholic beverages
+    </definition>
+    <definition name = "ajax"  author = "Kash">
+    Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    <definition name = "html"  author = "Abi">
+    The standard markup language for creating web pages and web applications
+    </definition>
+    <definition name = "css"  author = "TanTan">
+    A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name = "javascript" author = "Campbell">
+    A lightweight, interpreted programming language with first-class functions that adds interactivity to your website
+    </definition>
+    <definition name = "php" author = "QQ">
+    A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+</entries>';
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
